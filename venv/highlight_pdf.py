c8a295e5f558dcc3d4dc266d134f5bdce0502063
Alexey Vorobev <voralexey@gmail.com>
2022-03-14 13:30:24 +0100

    added comments

---
+++ venv/highlight_pdf.py
@@ -0,0 +1,32 @@
+import fitz # pip3 install pymupdf
+
+pdfIn = fitz.open("xgen-prism-dna-library-prep-kit-protocol.pdf") # pdf file to search through
+
+# either open a file with a list of search words *
+with open("no_no_words.txt") as file:
+    lines = [line.rstrip() for line in file]
+
+for page in pdfIn:
+    print(page)
+    # * or make a list of words
+    #lines = ["Able", "Allow", "Consistency"]
+    #text_instances = [page.search_for(text) for text in lines]
+    text_instances = [page.search_for(text) for text in lines]
+
+    # coordinates of each word found in PDF-page
+    print(text_instances)
+
+    # iterate through each instance for highlighting
+    for inst in text_instances:
+        annot = page.add_highlight_annot(inst)
+        # annot = page.add_rect_annot(inst)
+
+        ## Adding comment to the highlighted text
+        info = annot.info
+        info["title"] = "word_diffs"
+        info["content"] = "diffs"
+        annot.set_info(info)
+        annot.update()
+
+# Saving the PDF Output
+pdfIn.save("output.pdf")
